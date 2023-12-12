948ea5acc300fd77a687473b879e6ff34b273805
Khalilova <sharova-ira90@yandex.ru>
2023-12-12 23:46:17 +0300

    add DataGenerator

---
+++ src/test/java/data/DataGenerator.java
@@ -0,0 +1,53 @@
+package data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.time.format.DateTimeFormatter;
+
+import java.util.Random;
+
+public class DataGenerator {
+    private static final Faker faker = new Faker(new Locale("ru"));
+
+
+    public static String getNickname() {
+        String name = faker.name().firstName();
+        return name;
+    }
+
+    public static String getNumber(int lenght) {
+
+        return faker.number().digits(lenght);
+    }
+
+
+    private static LocalDate pastDate() {
+        LocalDate invalid = LocalDate.now().minusMonths(range(8, 24));
+        return invalid;
+    }
+
+
+
+    public static String getLocalDate() {
+        String day = LocalDate.now().format(DateTimeFormatter.ofPattern("dd"));
+        String month = LocalDate.now().format(DateTimeFormatter.ofPattern("MM"));
+        String year = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy"));
+        return (day+month+year);
+    }
+    public static String getPastDate() {
+        String day = LocalDate.now().format(DateTimeFormatter.ofPattern("dd"));
+        String month = pastDate().format(DateTimeFormatter.ofPattern("MM"));
+        String year = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy"));
+        return (day+month+year);
+    }
+
+
+    private static int range(int min, int max) {
+        Random random = new Random();
+        int range = min + random.nextInt(max - min + 1);
+        return range;
+    }
+}
--- src/test/java/first.java
+++ src/test/java/first.java
@@ -1,12 +1,9 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import data.DataGenerator;
 import org.junit.jupiter.api.BeforeEach;
 
-import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -14,6 +11,12 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class first {
+
+    boolean isVisible() {
+        $("#add-animal-page > div > div.modal.fade.modal-transition.show > div > div > div.modal-body.pt-0 > div > div:nth-child(2) > button").shouldBe(visible, Duration.ofSeconds(2));
+        return true;
+    }
+
     @DisplayName("Авторизация")
     void autoVet() {
         $("input[name=email]").setValue("tur@tur.tur");
@@ -29,12 +32,18 @@ public class first {
         $x("//button[contains(text(),'Найти другой объект')]").shouldBe(visible, Duration.ofSeconds(10));
     }
 
+    void activateRegistration() {
+        $x("//button/div[contains(text(),'Завершить')]").click();
+
+        $("#add-animal-page > div > div.modal.fade.modal-transition.show > div > div > div.modal-body.pt-0 > div > div:nth-child(2) > button").click();
+    }
+
     @DisplayName("Первичная регистрация")
     void firstRegistration() {
         // Выбор объекта
         $x("//button[contains(text(),'Найти другой объект')]").click();
         $x("//label[contains(text(),'ИНН')]").click();
-        $("#searchOwnerStep1 > div.row.align-items-end.mb-3.inn > div:nth-child(1) > input").setValue("4598700098");
+        $("#searchOwnerStep1 > div.row.align-items-end.mb-3.inn > div:nth-child(1) > input").setValue("0278039949");
         $x("//button[contains(text(),'Найти')]").click();
         $("div[class=item-descritpion]").click();
         $x("//button[contains(text(),'Выбрать')]").click();
@@ -50,7 +59,7 @@ public class first {
         $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
 
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").click();
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").setValue("484889594712577");
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").setValue(DataGenerator.getNumber(15));
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").pressEnter();
 
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(2) > div.col-5 > div > div > div > span > span.selection > span").click();
@@ -60,7 +69,7 @@ public class first {
         // Дата первичного маркирования
 
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input").click();
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue("10122023");
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue(DataGenerator.getLocalDate());
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").pressEnter();
 
         // Основное
@@ -69,59 +78,71 @@ public class first {
         $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
 
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(10) > div.col-6.mb-3.form-group.pb-3 > div > span > span.selection > span").click();
-        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Белая");
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Желтая");
         $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
 
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input").click();
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue("14.12.2022");
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue(DataGenerator.getPastDate());
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").pressEnter();
 
-        $("input[id=male]").click();
+        $("input[id=female]").click();
 
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(15) > div > div:nth-child(2) > div > input").setValue("Лола");
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(15) > div > div:nth-child(2) > div > input").setValue(DataGenerator.getNickname());
 
         // Содержание
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(17) > div.col-6.mb-3.form-group.pb-3 > div > span > span.selection > span").click();
-        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Пастбищный");
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Беспривязный");
         $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
 
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(19) > div.col-6.mb-3.form-group.pb-3 > div > span > span.selection > span").click();
-        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Навес");
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Загон");
         $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
 
         // Направление продуктивности
         $("#product_direction > div.col-6.mb-3.form-group.pb-3 > div > span > span.selection > span").click();
-        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Мясное");
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Племенное");
         $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
     }
 
     void nextRegistration() {
+
         // Идентификация
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col-5 > div > div > div > span > span.selection > span > button").click();
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").click();
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col-5 > div > div > div > span > span.selection > span").click();
         $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Чип");
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
+
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").click();
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").setValue(DataGenerator.getNumber(15));
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(1) > div.col > input").pressEnter();
+
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div > div.row.mb-3 > div:nth-child(2) > div > div:nth-child(2) > div.col-5 > div > div > div > span > span.selection > span").click();
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Холка");
         $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
 
         // Дата первичного маркирования
 
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input").click();
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue("10122023");
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue(DataGenerator.getLocalDate());
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(5) > div:nth-child(5) > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").pressEnter();
 
         // Дата Рождения
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input").click();
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue("14.12.2022");
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").setValue(DataGenerator.getPastDate());
         $("#addAnimalForm > div:nth-child(1) > div:nth-child(11) > div > div.col-6.mb-3.form-group.pb-3 > div > input.form-control.flatpickr.input.active").pressEnter();
 
         // Кличка
-        $("#addAnimalForm > div:nth-child(1) > div:nth-child(15) > div > div:nth-child(2) > div > input").setValue("Лола");
+        $("#addAnimalForm > div:nth-child(1) > div:nth-child(15) > div > div:nth-child(2) > div > input").setValue(DataGenerator.getNickname());
+
+        // Направление продуктивности
+        $("#product_direction > div.col-6.mb-3.form-group.pb-3 > div > span > span.selection > span").click();
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").setValue("Молочное");
+        $("body > span > span > span.select2-search.select2-search--dropdown > input").pressEnter();
+
+        activateRegistration();
+
     }
 
-    void activateRegistration() {
-        $x("//button/div[contains(text(),'Завершить')]").click();
-
-        $("#add-animal-page > div > div.modal.fade.modal-transition.show > div > div > div.modal-body.pt-0 > div > div:nth-child(2) > button").click();
-    }
 
     @BeforeEach
     void setup() {
@@ -131,7 +152,23 @@ public class first {
     }
 
     @Test
-    void auto() {
+    void regIndividualAnimals() {
+        int max = 20;
+        int count = 0;
+
+        autoVet();
+        getRegistrationPage();
+        firstRegistration();
+        activateRegistration();
+
+        do {
+            if ($("#add-animal-page > div > div.modal.fade.modal-transition.show > div > div > div.modal-body.pt-0 > div > div:nth-child(2) > button").isDisplayed()) {
+                $("#add-animal-page > div > div.modal.fade.modal-transition.show > div > div > div.modal-header > button").click();
+            } else
+                nextRegistration();
+            count++;
+        }
+        while (count < max);
 
     }
 }
